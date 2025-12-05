9911ad4b46e721220660fd2aedd24b3dfa7ba927
raghulsiva08 <raghulsiva27@gmail.com>
2025-12-05 11:01:23 +0530

    success

--- raghul08/raghul08/urls.py
+++ raghul08/raghul08/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for raghul project.
+URL configuration for raghul08 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
